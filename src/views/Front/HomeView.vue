6a51aa4239de94013824306da2d085a562fed54b
jacky123789456@gmail.com <jacky123789456@gmail.com>
2022-03-20 16:33:40 +0800

    測試版

--- src/views/Front/HomeView.vue
+++ src/views/Front/HomeView.vue
@@ -1,36 +1,33 @@
 <template>
   <Navbar />
   <div class="wrapper">
-      <div class="body">
-        <div class="container">
-          <router-view />
-        </div>
-      </div>
-      <div class="footer"></div>
   </div>
+  <div class="footer"></div>
 </template>
 
 <style lang="scss" scoped>
 .wrapper {
-      display: flex;
-      .body {
-        background-image: url('../../../public/images/Lobby/平台svg/lobby_bg.svg');
-        background-repeat: no-repeat;
-        background-position: center center;
-        position: fixed;
-        width: 100%;
-        height: calc(100% - .8rem);
-      }
-      .footer {
-        background-image: url('../../../public/images/Lobby/平台svg/lobby_bot_bar.svg');
-        background-repeat: no-repeat;
-        position: absolute;
-        width: 100%;
-        height: .6rem;
-        bottom: 0;
-        right: 0;
-      }
-  }
+  background-image: url('../../../public/images/Lobby/平台svg/lobby_bg.svg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+  background-size: cover;
+  position: fixed;
+  min-width: 100%;
+  min-height: 100vh;
+}
+.footer {
+  background-image: url('../../../public/images/Lobby/平台svg/lobby_bot_bar.svg');
+  background-repeat: no-repeat;
+  position: absolute;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: .6rem;
+  bottom: 0;
+  right: 0;
+}
 </style>
 
 <script>
