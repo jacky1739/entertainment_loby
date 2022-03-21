bc3d0c02fad08d8fe37cd741f8a185cc4e5db272
Jacky <jacky123789456@gmail.com>
2022-03-21 13:43:49 +0800

    圖片設定ok

--- src/views/Front/TestView.vue
+++ src/views/Front/TestView.vue
@@ -1,3 +1,26 @@
 <template>
-  <h2>這是測試</h2>
+    <ul class="games">
+        <li class="games__first">
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版長_金蟬捕魚.png" alt=""></a>
+        </li>
+        <li class="games__item">
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_龍虎鬥.png" alt=""></a>
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_龍虎鬥.png" alt=""></a>
+        </li>
+        <li class="games__item">
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_龍虎鬥.png" alt=""></a>
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_龍虎鬥.png" alt=""></a>
+        </li>
+    </ul>
 </template>
+
+<style lang="scss" scoped>
+.games {
+  display: flex;
+  height: 90%;
+  &__item {
+    height: 50%;
+    margin: 0 .2rem 1rem 0;
+  }
+}
+</style>
