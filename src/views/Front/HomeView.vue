018a233c6cdc7bb207d8ffd1263875554fb11b77
Jacky <jacky123789456@gmail.com>
2022-03-21 13:55:05 +0800

    調整檔案位置

--- src/views/Front/HomeView.vue
+++ src/views/Front/HomeView.vue
@@ -13,28 +13,6 @@
   flex-direction: column;
 }
 .wrapper {
-<<<<<<< HEAD
-      display: flex;
-      .body {
-        background-image: url('../../../public/images/Lobby/平台svg/lobby_bg.svg');
-        background-repeat: no-repeat;
-        background-position: center center;
-        background-size: cover;
-        position: fixed;
-        width: 100%;
-        height: calc(100% - .8rem);
-      }
-      .footer {
-        background-image: url('../../../public/images/Lobby/平台svg/lobby_bot_bar.svg');
-        background-repeat: no-repeat;
-        position: absolute;
-        width: 100%;
-        height: .6rem;
-        bottom: 0;
-        right: 0;
-      }
-  }
-=======
   background-image: url('../../../public/images/Lobby/平台svg/lobby_bg.svg');
   background-repeat: no-repeat;
   background-position: center;
@@ -46,7 +24,6 @@
   display: flex;
   align-items: center;
 }
->>>>>>> test
 </style>
 
 <script>
--- src/views/Front/IndexView.vue
+++ src/views/Front/IndexView.vue
@@ -1,64 +1,35 @@
 <template>
-  <ul class="games">
-    <li class="games__firstGame">
-      <a href="#"><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版長_金蟬捕魚.png" alt=""></a>
-    </li>
-    <li>
-      <ul>
-        <li class="games__icon">
-          <a href="#"><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_龍虎鬥.png" alt=""></a>
+    <ul class="games">
+        <li class="games__first">
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版長_金蟬捕魚.png" alt=""></a>
         </li>
-        <li class="games__icon">
-          <a href="#"><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_五星宏輝.png" alt=""></a>
+        <li class="games__item">
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_龍虎鬥.png" alt=""></a>
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_五星宏輝.png" alt=""></a>
         </li>
-      </ul>
-    </li>
-    <li>
-      <ul>
-        <li class="games__icon">
-          <a href="#"><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_五星宏輝.png" alt=""></a>
+        <li class="games__item">
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_五星宏輝.png" alt=""></a>
+            <a href=""><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_龍虎鬥.png" alt=""></a>
         </li>
-        <li class="games__icon">
-          <a href="#"><img src="../../../public/images/Lobby/遊戲icon/png/新icon公版_龍虎鬥.png" alt=""></a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-  <div class="next__page">
-    <a href="#"><img src="../../../public/images/Lobby/平台svg/lobby_next_page.svg" alt=""></a>
-  </div>
+    </ul>
+    <div class="next__page">
+        <a href="#"><img src="../../../public/images/Lobby/平台svg/lobby_next_page.svg" alt=""></a>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-  .games {
-    display: flex;
-    margin-top: 2.5%;
-    height: 100%;
-    width: 100%;
-    &__firstGame {
-      min-width: 4.1rem;
-      height: 100%;
-    }
-    &__icon {
-      width: 3.5rem;
-      height: 100%;
-      margin: 0 .15rem .25rem 0;
-    }
-  }
-  .next__page {
-    width: 1rem;
-    height: 1rem;
-    position: absolute;
-    right: 32px;
-    top: 40%;
-  }
-</style>
-
-<script>
-export default {
-  data () {
-    return {
-    }
+.games {
+  display: flex;
+  height: 90%;
+  &__item {
+    height: 50%;
+    margin: 0 .2rem 1rem 0;
   }
 }
-</script>
+.next__page {
+  width: 1rem;
+  height: auto;
+  position: absolute;
+  right: 0;
+}
+</style>
