209af3397750aff72dd6300c9377ffcd73e88ca3
Jacky <jacky123789456@gmail.com>
2022-03-23 16:51:06 +0800

    modal 設定大概

--- src/components/ModalItem.vue
+++ src/components/ModalItem.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="popUp__Setting">
-    <div class="popUp__Setting__bg">
-      <!-- <p>這是modal</p> -->
+  <div class="modal">
+    <div class="modal__cancel">
+      <a href="#"><img src="../../public/images/Lobby/平台svg/ibfo_bdm_setting_exit_normal.svg" alt=""></a>
+    </div>
+    <div class="modal__bg">
+      <h2 class="modal__title">設定</h2>
+      <div class="modal__btn">
+        <p class="modal__btn__text">音效</p>
+        <p class="modal__btn__text">音效</p>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.popUp__Setting {
+.modal {
   width: 400px;
   position: absolute;
   top:50%;
@@ -20,7 +27,37 @@
     background-size: contain;
     width: 6rem;
     height: 4rem;
-
+    padding: 0 1rem;
+  }
+  &__title {
+    line-height: .9rem;
+    text-align: center;
+    font-size: .4rem;
+    color: white;
+  }
+  &__cancel {
+    position: absolute;
+    width: .88rem;
+    height: auto;
+    top: -18px;
+    right: -223px;
+  }
+  &__btn {
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    height: calc(100% - .9rem);
+    &__text {
+      line-height: 1rem;
+      font-size: .4rem;
+    }
+    &__switch {
+      background-image: url('../../public/images/Lobby/平台svg/btn_bgm_on.svg');
+      background-repeat: no-repeat;
+      background-size: cover;
+      width: .9rem;
+      height: .2rem;
+    }
   }
 }
 </style>
